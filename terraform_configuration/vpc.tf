7d52997fe9955dca21716e9d875fbcdf2b2e6a1d
shin20090317 <shintaro060776@gmail.com>
2023-11-29 23:57:36 +0900

    7

--- terraform_configuration/vpc.tf
+++ terraform_configuration/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "next" {
     cidr_block = "172.16.0.0/16"
     enable_dns_support = true
-    enable_dbs_hostnames = true
+    enable_dns_hostnames = true
 
     tags = {
         Name = "next"
