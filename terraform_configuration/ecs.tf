38d0e0b1b831276235fbeb4d0d5c669e0a12d9a9
shin20090317 <shintaro060776@gmail.com>
2023-12-01 03:59:29 +0900

    48

--- terraform_configuration/ecs.tf
+++ terraform_configuration/ecs.tf
@@ -133,21 +133,6 @@ resource "aws_ecs_service" "next_service" {
   desired_count = 1
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = var.ecs_execution_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = "sts:AssumeRole",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
