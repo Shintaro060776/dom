e23d5710ae6d89a29fe1e10ac76e59c433a49591
shin20090317 <shintaro060776@gmail.com>
2023-12-01 22:55:04 +0900

    60

--- terraform_configuration/vpc.tf
+++ terraform_configuration/vpc.tf
@@ -104,3 +104,22 @@ resource "aws_vpc_endpoint" "ecr_api" {
 
   security_group_ids = [aws_security_group.vpc_endpoint.id]
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.next.id
+  service_name = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_route_table.next.id]
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id       = aws_vpc.next.id
+  service_name = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [aws_subnet.next.id, aws_subnet.next2.id]
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.vpc_endpoint.id]
+}
