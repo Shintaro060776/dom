89ee79f22d45517fe67e5246d78588d63745d283
shin20090317 <shintaro060776@gmail.com>
2023-12-09 01:20:30 +0900

    20

---
+++ blog/myapp/src/3.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './3.css';
+
+const BlogArticle3 = () => {
+    return (
+        <div className='App'>
+            <img src='/blog/20231004_18_26_0.png' alt='second' className='header-image' />
+            <div className='page-title'>
+                <h1>考え中</h1>
+            </div>
+            <div className='page-date'>
+                <p>2023/01/14</p>
+            </div>
+            <div className='paragraph'>
+                <p>
+                    実験サイトで作った、機械学習を用いたサービスの概要を、以下に記載します。<br /><br />
+
+                    <span className="highlight">①動くヌコ</span><br /><br />
+                    URL : http://neilaeden.com/predict/<br /><br />
+                    アプリの概要：ユーザーがフォーム上に、ネコの絵を書いて、その書いた絵を、アニメーション(動かす)させるというWebアプリケーション<br /><br />
+                    難易度：★★★★☆<br /><br />
+                    雑感 : Web上に、なかなか手書きのネコの画像が無いから、完成度は、そこまで高くないかな。推論処理は動くし、エラーも出ていないから、ま、いいか、という感じ。
+                    何故、ネコだけにしたかというと、ネコ以外の学習データ(犬、鳥、魚など)も取り入れると、機械学習(AI)サーバーの学習時間が増大して、費用が高くなるから、限定的にしたという経緯です。スマホで、フォームに絵を書けるけど、書きづらいから、PCのブラウザから、どうぞ
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default BlogArticle3;
--- blog/myapp/src/App.js
+++ blog/myapp/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AboutPage from './AboutPage';
 import BlogArticle from './1';
+import BlogArticle2 from './2';
+import BlogArticle3 from './3';
 
 const blogEntries = [
   {
@@ -10,6 +12,20 @@ const blogEntries = [
     date: "2023/01/03",
     thumbnail: "20231006_18_13_0.png",
     path: "/blog1/1"
+  },
+  {
+    id: 2,
+    title: "機械学習を用いた実験的Webアプリケーションについて",
+    date: "2023/01/09",
+    thumbnail: "20231016_12_05_0.png",
+    path: "/blog2/2"
+  },
+  {
+    id: 3,
+    title: "考え中",
+    date: "2023/01/14",
+    thumbnail: "20231004_18_26_0.png",
+    path: "/blog3/3"
   },
 ];
 
@@ -33,6 +49,8 @@ function App() {
         <Routes>
           <Route path="/about" element={<AboutPage />} />
           <Route path="/blog1/1" element={<BlogArticle />} />
+          <Route path="/blog2/2" element={<BlogArticle2 />} />
+          <Route path="/blog3/3" element={<BlogArticle3 />} />
           <Route path="/blog" element={
             <main className="blog-container">
               <h2 className="blog-title">Blog</h2>
