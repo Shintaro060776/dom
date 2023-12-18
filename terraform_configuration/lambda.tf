c866b814e1a7f8bde23c601d6cee2a499cfa34b1
shin20090317 <shintaro060776@gmail.com>
2023-12-18 22:55:29 +0900

    68

--- terraform_configuration/lambda.tf
+++ terraform_configuration/lambda.tf
@@ -69,7 +69,7 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 }
 
 resource "aws_lambda_layer_version" "requests_layer" {
-  filename   = "/home/runner/work/dom/dom/lambda-layer/requests_layer.zip"
+  filename   = "/home/runner/work/dom/dom/lambda-layer/python_layer.zip"
   layer_name = "requests-layer"
   compatible_runtimes = ["python3.8"]
   source_code_hash = filebase64sha256("/home/runner/work/dom/dom/lambda-layer/python_layer.zip")
