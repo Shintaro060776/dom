cc5aeed6f24acbb14efc81ecc7e4c9ec42c10ad0
shin20090317 <shintaro060776@gmail.com>
2023-12-25 21:18:16 +0900

    106

--- realtime_backend/server.js
+++ realtime_backend/server.js
@@ -2,37 +2,22 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+const port = process.env.PORT || 4000;
+
 app.use(express.json());
+
+const API_GATEWAY_URL = 'https://6xc8jru0di.execute-api.ap-northeast-1.amazonaws.com/prod/realtime';
 
 app.post('/api/realtime', async (req, res) => {
     try {
-        const { user_input } = req.body;
-
-        const lambdaResponse = await axios.post(
-            'https://6xc8jru0di.execute-api.ap-northeast-1.amazonaws.com/prod/realtime',
-            { user_input },
-            { timeout: 5000 } // example timeout
-        );
-
-        res.status(200).json(lambdaResponse.data);
+        const response = await axios.post(API_GATEWAY_URL, req.body);
+        res.json(response.data);
     } catch (error) {
-        console.error("Error details:", {
-            message: error.message,
-            status: error.response?.status,
-            headers: error.response?.headers,
-            data: error.response?.data,
-        });
-
-        const clientErrorMessage = error.response
-            ? `Error from server: ${error.response.status} ${error.response.statusText}`
-            : 'Internal server error';
-
-        res.status(500).json({ message: clientErrorMessage });
+        res.status(500).json({ message: 'Internal Server Error', error: error });
     }
 });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
