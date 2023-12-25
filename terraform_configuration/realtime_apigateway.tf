876e2ec6a344bec495f2a3135588e7cbbff6e520
shin20090317 <shintaro060776@gmail.com>
2023-12-25 22:02:45 +0900

    108

--- terraform_configuration/realtime_apigateway.tf
+++ terraform_configuration/realtime_apigateway.tf
@@ -44,7 +44,7 @@ resource "aws_api_gateway_integration_response" "realtime_integration_response_2
 resource "aws_api_gateway_deployment" "realtime_deployment" {
     depends_on  = [aws_api_gateway_integration.realtime_integration]
     rest_api_id = aws_api_gateway_rest_api.realtime.id
-    stage_name  = "prod"
+    stage_name  = "test"
 }
 
 
@@ -69,7 +69,7 @@ resource "aws_api_gateway_method_settings" "settings_realtime" {
 }
 
 resource "aws_api_gateway_stage" "realtime" {
-    stage_name = "prod"
+    stage_name = "test"
     rest_api_id = aws_api_gateway_rest_api.realtime.id
     deployment_id = aws_api_gateway_deployment.realtime_deployment.id
 
