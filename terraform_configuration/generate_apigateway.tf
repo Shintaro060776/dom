09862b58fb0d733aca6d65a6751ec3a43fae9194
shin20090317 <shintaro060776@gmail.com>
2024-01-01 19:32:05 +0900

    138

--- terraform_configuration/generate_apigateway.tf
+++ terraform_configuration/generate_apigateway.tf
@@ -50,7 +50,7 @@ resource "aws_cloudwatch_log_group" "generate_log_group" {
 }
 
 resource "aws_api_gateway_stage" "generate" {
-  stage_name    = "prod"
+#   stage_name    = "prod"
   rest_api_id   = aws_api_gateway_rest_api.generate_api.id
   deployment_id = aws_api_gateway_deployment.generate_deployment.id
 
