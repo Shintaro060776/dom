702bd1d38f590ba225f5a9d4b8afd8e2686030bd
shin20090317 <shintaro060776@gmail.com>
2024-01-01 18:41:28 +0900

    133

--- terraform_configuration/generate_apigateway.tf
+++ terraform_configuration/generate_apigateway.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "generate_api" {
 
 resource "aws_api_gateway_resource" "generate_resource" {
     rest_api_id = aws_api_gateway_rest_api.generate_api.id
-    parent_id = aws_api_gateway_rest_api.generate_api.root_resource.id
+    parent_id = aws_api_gateway_rest_api.generate_api.root_resource_id
     path_part = "generate"
 }
 
