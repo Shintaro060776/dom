5a51c435710735e8ec3ff778a5d834bc31a8ae50
shin20090317 <shintaro060776@gmail.com>
2024-01-11 19:11:00 +0900

    159

--- terraform_configuration/claim_apigateway.tf
+++ terraform_configuration/claim_apigateway.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "claim_api" {
 }
 
 resource "aws_api_gateway_resource" "api_resource" {
-    rest_api_id = aws_api_gateway_rest_api.clamin_api.id
+    rest_api_id = aws_api_gateway_rest_api.claim_api.id
     parent_id = aws_api_gateway_rest_api.claim_api.root_resource_id
     path_part = "claim_handler"
 }
