37601c78072f2961430394d2c9d9dfa1802f0cc1
shin20090317 <shintaro060776@gmail.com>
2024-01-14 21:05:00 +0900

    180

---
+++ dalle/myapp/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [inputText, setInputText] = useState('');
+
+  const fetchImage = async () => {
+    try {
+      const response = await axios.post('http://3.112.43.184/api/dalle', {
+        text: inputText
+      });
+      setImageUrl(response.data.imageUrl);
+    } catch (error) {
+      console.error("Error fetching image: ", error);
+    }
+  };
+
+  return (
+    <div className='app'>
+      <header className='header'>
+        <h2>Generate Image by Dall・E</h2>
+        <a href='http://3.112.43.184/'>BackToTopPage</a>
+      </header>
+      <div className='body'>
+        <div className='input-container'>
+          <input
+            type='text'
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder='Please write what you wanna generate'
+          />
+          <button onClick={fetchImage} className='fetch-button'>Generate Image</button>
+          <div className='image-container'>
+            {imageUrl && <img src={imageUrl} alt='Generated' />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ dalle_backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 7000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Nodejs server is running');
+});
+
+app.post('/api/dalle', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await axios.post('https://vcfxizwoc4.execute-api.ap-northeast-1.amazonaws.com/prod/openai', { prompt: prompt });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error while calling OpenAI API');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
