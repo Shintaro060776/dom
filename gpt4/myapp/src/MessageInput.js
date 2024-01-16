4e0be59dfa522ccf4055cd37c1f4530c76c8e626
shin20090317 <shintaro060776@gmail.com>
2024-01-16 20:44:48 +0900

    202

--- gpt4/myapp/src/MessageInput.js
+++ gpt4/myapp/src/MessageInput.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 function MessageInput({ onSendMessage }) {
     const [text, setText] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (text) {
             try {
-                const response = axios.post('http://3.112.43.184/api/gpt4', { message: text });
+                const response = await axios.post('http://3.112.43.184/api/gpt4', { message: text });
                 onSendMessage(text, response.data);
             } catch (error) {
                 console.error('Error sending message:', error);
@@ -18,10 +18,13 @@ function MessageInput({ onSendMessage }) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-            <button type="submit">Send</button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                <button type="submit">Send</button>
+            </form>
+            <a href='http://3.112.43.184/'>トップページに戻る</a>
+        </div>
     );
 }
 
