947f2fbfdabb0263ebced6008d4f32a22d1c75e4
shin20090317 <shintaro060776@gmail.com>
2024-01-17 17:18:47 +0900

    209

--- gpt4/myapp/src/App.js
+++ gpt4/myapp/src/App.js
@@ -13,7 +13,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className='app-container'>
       <MessageList messages={messages} />
       <MessageInput onSendMessage={handleSendMessage} />
     </div>
