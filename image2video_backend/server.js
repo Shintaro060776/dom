cab5ba9d35364474b298729708dd2fee9f202e70
shin20090317 <shintaro060776@gmail.com>
2024-01-20 01:48:40 +0900

    239

--- image2video_backend/server.js
+++ image2video_backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const multer = require('multer');
+const FormData = require('form-data');
 const app = express();
 
 const upload = multer();
