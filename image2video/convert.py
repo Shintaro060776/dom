b04496f1d306c50d5872beb69482a96b784fe121
shin20090317 <shintaro060776@gmail.com>
2024-01-24 18:15:04 +0900

    283

--- image2video/convert.py
+++ image2video/convert.py
@@ -7,7 +7,7 @@ def resize_image(input_image_path, output_image_path, size=(768, 768)):
         img.save(output_image_path)
 
 
-input_path = "/dom/image2video/20240119_12_38_0.png"
-output_path = "/dom/image2video/20240119_12_38_0_convert.png"
+input_path = "/dom/image2video/20240118_07_45_0.png"
+output_path = "/dom/image2video/20240118_07_45_0_convert.png"
 
 resize_image(input_path, output_path)
