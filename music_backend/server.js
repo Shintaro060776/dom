827c3fc3e95e353f54f4898746e6ec7b17334510
shin20090317 <shintaro060776@gmail.com>
2024-03-06 23:05:27 +0900

    338

--- music_backend/server.js
+++ music_backend/server.js
@@ -7,18 +7,12 @@ const port = 16000;
 
 app.use(bodyParser.json());
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'ap-northeast-1'
-});
-
 const apiEndpoint1 = 'https://12zk6rip6j.execute-api.ap-northeast-1.amazonaws.com/prod/music1';
 const apiEndpoint2 = 'https://poa6thi2jh.execute-api.ap-northeast-1.amazonaws.com/prod/music3';
 
 app.post('/api/music-presigned', async (req, res) => {
     try {
-        const response = await axios.post('${apiEndpoint1}', req.body);
+        const response = await axios.post(`${apiEndpoint1}`, req.body);
         res.json(response.data);
     } catch (error) {
         console.error(error);
@@ -28,7 +22,7 @@ app.post('/api/music-presigned', async (req, res) => {
 
 app.get('/api/music', async (req, res) => {
     try {
-        const response = await axios.get('${apiEndpoint2}');
+        const response = await axios.get(`${apiEndpoint2}`);
         res.json(response.data);
     } catch (error) {
         console.error(error);
