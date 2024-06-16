46ee49d2f6cbbfcb91fcf452e586de5d25f1470f
shin20090317 <shintaro060776@gmail.com>
2024-06-16 11:29:25 +0900

    392

--- terraform_configuration/capture1_apigateway.tf
+++ terraform_configuration/capture1_apigateway.tf
@@ -31,9 +31,9 @@ resource "aws_api_gateway_method_response" "capture1_200" {
     http_method = aws_api_gateway_method.capture1.http_method
     status_code = "200"
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,HEAD,OPTIONS'",
-        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin"  = true
     }
 }
 
@@ -43,9 +43,9 @@ resource "aws_api_gateway_integration_response" "capture1_200" {
     http_method = aws_api_gateway_method.capture1.http_method
     status_code = aws_api_gateway_method_response.capture1_200.status_code
     response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,HEAD,OPTIONS'",
-        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin"  = true
     }
 }
 
