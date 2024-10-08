f96d6488763cd7e1a64dc09e05ac7670aab541d0
shin20090317 <shintaro060776@gmail.com>
2024-10-08 22:30:30 +0900

    428

--- terraform_configuration/time_management_apigateway.tf
+++ terraform_configuration/time_management_apigateway.tf
@@ -34,46 +34,69 @@ resource "aws_api_gateway_method" "time_management_options_method" {
     authorization = "NONE"
 }
 
+# OPTIONSメソッドの統合
 resource "aws_api_gateway_integration" "time_management_options_integration" {
-    rest_api_id             = aws_api_gateway_rest_api.time_management_api_gateway.id
-    resource_id             = aws_api_gateway_resource.time_management_api_resource.id
-    http_method             = aws_api_gateway_method.time_management_options_method.http_method
-    type                    = "MOCK"  # OPTIONSリクエスト用にMOCKレスポンスを使用
+    rest_api_id = aws_api_gateway_rest_api.time_management_api_gateway.id
+    resource_id = aws_api_gateway_resource.time_management_api_resource.id
+    http_method = aws_api_gateway_method.time_management_options_method.http_method
+    type        = "MOCK"
 
     request_templates = {
         "application/json" = "{\"statusCode\": 200}"
     }
+}
 
-    integration_response {
-        status_code = "200"
+# OPTIONSメソッドのレスポンス設定
+resource "aws_api_gateway_method_response" "time_management_options_method_response" {
+    rest_api_id = aws_api_gateway_rest_api.time_management_api_gateway.id
+    resource_id = aws_api_gateway_resource.time_management_api_resource.id
+    http_method = aws_api_gateway_method.time_management_options_method.http_method
+    status_code = "200"
 
-        response_parameters = {
-            "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
-            "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
-            "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-        }
-
-        response_templates = {
-            "application/json" = ""
-        }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true
+        "method.response.header.Access-Control-Allow-Methods" = true
+        "method.response.header.Access-Control-Allow-Origin"  = true
     }
 }
 
-# CORS用のレスポンスヘッダーをPOSTメソッドにも追加
+resource "aws_api_gateway_integration_response" "time_management_options_integration_response" {
+    rest_api_id = aws_api_gateway_rest_api.time_management_api_gateway.id
+    resource_id = aws_api_gateway_resource.time_management_api_resource.id
+    http_method = aws_api_gateway_method.time_management_options_method.http_method
+    status_code = "200"
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+        "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    }
+}
+
+# POSTメソッドのレスポンスにCORSヘッダーを追加
 resource "aws_api_gateway_method_response" "time_management_post_method_response" {
     rest_api_id = aws_api_gateway_rest_api.time_management_api_gateway.id
     resource_id = aws_api_gateway_resource.time_management_api_resource.id
     http_method = "POST"
     status_code = "200"
 
-    response_models = {
-        "application/json" = "Empty"
-    }
-
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = true
         "method.response.header.Access-Control-Allow-Methods" = true
         "method.response.header.Access-Control-Allow-Origin"  = true
+    }
+}
+
+resource "aws_api_gateway_integration_response" "time_management_post_integration_response" {
+    rest_api_id = aws_api_gateway_rest_api.time_management_api_gateway.id
+    resource_id = aws_api_gateway_resource.time_management_api_resource.id
+    http_method = aws_api_gateway_method.time_management_api_method.http_method
+    status_code = "200"
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+        "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
     }
 }
 
